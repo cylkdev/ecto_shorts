e76e17925feeedcce658dc88e6208d58e703b491
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2026-03-11 17:27:24 -0400

    wip-3

--- lib/ecto_shorts/common_filters/update.ex
+++ lib/ecto_shorts/common_filters/update.ex
@@ -1,31 +1,15 @@
 defmodule EctoShorts.CommonFilters.Update do
   alias EctoShorts.Compiler
+  alias EctoShorts.Utils
+
   alias Ecto.Query
-
   require Ecto.Query
-
-  # @supported_ops [:set, :inc]
 
   {_, binding_patterns} =
     Compiler.query_binding_contracts(__MODULE__, positions: 10)
 
   def build_query(:update, _source, query, selected_binding, term, _opts) do
-    apply_update_expr(query, selected_binding, normalize_update_entries(term))
-  end
-
-  defp normalize_update_entries(entries)
-       when (is_map(entries) and not is_struct(entries)) or is_list(entries) do
-    Enum.map(entries, fn
-      {op, value} when is_map(value) and not is_struct(value) ->
-        {op, Map.to_list(value)}
-
-      {op, value} ->
-        {op, value}
-    end)
-  end
-
-  defp normalize_update_entries(entries) do
-    entries
+    apply_update_expr(query, selected_binding, Utils.normalize_input(term))
   end
 
   for {quoted_binding_head, quoted_binding_body} <- binding_patterns do
