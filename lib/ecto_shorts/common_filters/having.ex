4919008ee1821e4bad7f7a883ba82a0ca45f989f
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2026-03-12 05:53:05 -0400

    wip-3

--- lib/ecto_shorts/common_filters/having.ex
+++ lib/ecto_shorts/common_filters/having.ex
@@ -5,37 +5,52 @@ defmodule EctoShorts.CommonFilters.Having do
   alias Ecto.Query
   require Ecto.Query
 
-  {_, binding_patterns} =
-    Compiler.query_binding_contracts(10, __MODULE__)
+  {_, binding_patterns} = Compiler.query_binding_contracts(10, __MODULE__)
 
-  def build_query(_filter, _source, query, _selected_binding, nil, _opts), do: query
+  def build_query(filter, _source, query, _selected_binding, nil, _opts)
+      when filter in [:having, :or_having] do
+    query
+  end
 
-  def build_query(filter, source, query, selected_binding, term, opts) do
-    dyn =
-      if is_struct(term, Ecto.Query.DynamicExpr) do
-        term
-      else
-        Postgres.build_dynamic(
-          source,
-          selected_binding,
-          term,
-          opts
-        )
-      end
+  def build_query(:having, source, query, selected_binding, term, opts) do
+    dyn = build_dynamic(source, selected_binding, term, opts)
+    build_having(query, selected_binding, dyn)
+  end
 
-    build_having(filter, query, selected_binding, dyn)
+  def build_query(:or_having, source, query, selected_binding, term, opts) do
+    dyn = build_dynamic(source, selected_binding, term, opts)
+    build_or_having(query, selected_binding, dyn)
+  end
+
+  defp build_dynamic(source, selected_binding, term, opts) do
+    if is_struct(term, Ecto.Query.DynamicExpr) do
+      term
+    else
+      Postgres.build_dynamic(
+        source,
+        selected_binding,
+        term,
+        opts
+      )
+    end
   end
 
   for {quoted_binding_head, quoted_binding_body} <- binding_patterns do
-    defp build_having(:having, query, unquote(quoted_binding_head), dyn) do
+    defp build_having(query, unquote(quoted_binding_head), dyn) do
       Query.having(
         query,
         [unquote_splicing(quoted_binding_body)],
         ^dyn
       )
     end
+  end
 
-    defp build_having(:or_having, query, unquote(quoted_binding_head), dyn) do
+  defp build_having(query, _selected_binding, dyn) do
+    Query.having(query, ^dyn)
+  end
+
+  for {quoted_binding_head, quoted_binding_body} <- binding_patterns do
+    defp build_or_having(query, unquote(quoted_binding_head), dyn) do
       Query.or_having(
         query,
         [unquote_splicing(quoted_binding_body)],
@@ -44,13 +59,7 @@ defmodule EctoShorts.CommonFilters.Having do
     end
   end
 
-  defp build_having(filter, query, _selected_binding, dyn) do
-    case filter do
-      :having ->
-        Query.having(query, ^dyn)
-
-      :or_having ->
-        Query.or_having(query, ^dyn)
-    end
+  defp build_or_having(query, _selected_binding, dyn) do
+    Query.or_having(query, ^dyn)
   end
 end
