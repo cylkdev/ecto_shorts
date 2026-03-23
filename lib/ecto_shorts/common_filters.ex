eecdfb90e9f751d98d137e619d109e5036b0370d
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2026-03-23 13:46:31 -0400

    Guard against nil dynamic expressions before passing to Ecto where/having query clauses

--- lib/ecto_shorts/common_filters.ex
+++ lib/ecto_shorts/common_filters.ex
@@ -647,11 +647,15 @@ defmodule EctoShorts.CommonFilters do
 
   defp dispatch_build_query(filter, source, query, selected_binding, term, opts)
        when filter in @predicate_filters do
-    dyn = DynamicBuilders.build_dynamic(source, selected_binding, term, opts)
+    case DynamicBuilders.build_dynamic(source, selected_binding, term, opts) do
+      nil ->
+        query
 
-    case filter do
-      :where -> Query.where(query, ^dyn)
-      :or_where -> Query.or_where(query, ^dyn)
+      dyn ->
+        case filter do
+          :where -> Query.where(query, ^dyn)
+          :or_where -> Query.or_where(query, ^dyn)
+        end
     end
   end
 
